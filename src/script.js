e3ce75a8c4d3b9f9e996fc13e5a8c0dc56aaca36
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-02-08 21:42:02 +0530

    initial commit

---
+++ src/script.js
@@ -0,0 +1,21 @@
+const imgContainer = document.querySelector(".image-container");
+const prev_btn = document.querySelector("#prev")
+const next_btn = document.querySelector("#next")
+
+let x = 0;
+prev_btn.addEventListener('click' , ()=>{
+    x +=90;
+    updateGallary();
+
+})
+
+next_btn.addEventListener('click' , ()=>{
+    x -=90;
+    updateGallary();
+
+})
+
+
+function updateGallary(){
+  imgContainer.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+}
